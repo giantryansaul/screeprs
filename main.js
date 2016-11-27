18737b539c75e51d8c62ce85bb014604604833b8
Ryan Saul <seccom@gmail.com>
2016-11-27 09:39:24 -0800

    forgot a var

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var common = require('common');
 
 module.exports.loop = function () {
 
-    rooms = common.getRooms();
+    var rooms = common.getRooms();
     for (var i = 0; i < rooms.length; i++) {
         base.buildStructures(rooms[i]);
         base.buildWorkers(rooms[i]);
